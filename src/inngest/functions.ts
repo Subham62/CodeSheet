77650f7b1a1554b6a5d5a249a7be11f7d0c83cbf
Subham62 <subhamchakraborty2002@gmail.com>
2025-09-16 18:10:23 +0530

    06: e2b sandboxes

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,23 +1,36 @@
+import { Sandbox } from "@e2b/code-interpreter";
 import { gemini, createAgent } from "@inngest/agent-kit";
 
 import { inngest } from "./client";
+import { getSandbox } from "./utils";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event }) => {
+  async ({ event, step }) => {
+    const sandboxId = await step.run("get-sandbox-id", async () => {
+      const sandbox = await Sandbox.create("codesheet-nextjs-test");
+      return sandbox.sandboxId;
+    });
     const codeAgent = createAgent({
       name: "code-agent",
-      system: "You are an expert next.js developer.  You write readable, maintainable code . You write simple Next.js & React snippets.",
+      system:
+        "You are an expert next.js developer.  You write readable, maintainable code . You write simple Next.js & React snippets.",
       // model: gemini({ model: "gemini-2.0-flash", apiKey: process.env.GEMINI_API_KEY }),
-      model: gemini({ model: "gemini-2.0-flash"}),
+      model: gemini({ model: "gemini-2.0-flash" }),
     });
 
-    
     // Run the agent with an input.  This automatically uses steps
     // to call your AI model.
-    const { output } = await codeAgent.run(`Write the following snippet: ${event.data.val}`);
-    console.log(output);
-    return {output};
-  },
+    const { output } = await codeAgent.run(
+      `Write the following snippet: ${event.data.val}`
+    );
+
+    const sandboxUrl = await step.run("get-sandbox-url", async () => {
+      const sandbox = await getSandbox(sandboxId);
+      const host = sandbox.getHost(3000);
+      return `https://${host}`;
+    });
+    return { output, sandboxUrl };
+  }
 );
