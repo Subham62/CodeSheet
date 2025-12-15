510e150de744779e1fccf78a08c9e38ac0b146fd
Subham62 <subhamchakraborty2002@gmail.com>
2025-12-15 19:48:48 +0530

    19: bug fixes(added custom error UI for message container)

---
+++ src/app/error.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+import React from 'react'
+
+const ErrorPage = () => {
+  return (
+    <div>Global ErrorPage</div>
+  )
+}
+
+export default ErrorPage
+
+// This is the last line of defence if something going wrong
--- src/modules/projects/ui/components/usage.tsx
+++ src/modules/projects/ui/components/usage.tsx
@@ -3,6 +3,7 @@ import { useAuth } from "@clerk/nextjs";
 import { formatDuration, intervalToDuration } from "date-fns";
 import { CrownIcon } from "lucide-react";
 import Link from "next/link";
+import { useMemo } from "react";
 
 interface Props {
   points: number;
@@ -11,7 +12,23 @@ interface Props {
 
 export const Usage = ({ points, msBeforeNext }: Props) => {
   const { has } = useAuth();
-  const hasProAccess = has?.({ plan: "pro" });      //  use the "slug" value (of clerk billing)
+  const hasProAccess = has?.({ plan: "pro" }); //  use the "slug" value (of clerk billing)
+
+  const resetTime = useMemo(() => {
+    try {
+      return formatDuration(
+        intervalToDuration({
+          start: new Date(),
+          end: new Date(Date.now() + msBeforeNext),
+        }),
+        { format: ["months", "days", "hours"] }
+      );
+    } catch (error) {
+      console.log("Error formatting duration", error);
+      return "unknown";
+    }
+  }, [msBeforeNext]);
+
   return (
     <div className="rounded-t-xl bg-background border border-b-0 p-2.5">
       <div className="flex items-center gap-x-2">
@@ -20,14 +37,7 @@ export const Usage = ({ points, msBeforeNext }: Props) => {
             {points} {hasProAccess ? "" : "free"} credits remaining
           </p>
           <p className="text-xs text-muted-foreground ">
-            Resets in{" "}
-            {formatDuration(
-              intervalToDuration({
-                start: new Date(),
-                end: new Date(Date.now() + msBeforeNext),
-              }),
-              { format: ["months", "days", "hours"] }
-            )}
+            Resets in{" "}{resetTime}
           </p>
         </div>
         {!hasProAccess && (
